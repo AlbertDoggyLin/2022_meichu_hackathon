e777ad44cba3b1e323c9500dcc33dda9f93cf4a1
chenlitw <ab0897867564534231@gmail.com>
2022-10-22 18:23:30 +0800

    feat: fake login (get user_id and type)

--- Server/app.js
+++ Server/app.js
@@ -19,6 +19,28 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 app.get('/',(req,res,next)=>{res.json({message:'hello world'})})
 
+app.post("/api/login/buyer", (req,res,next)=>{
+  DB.query("SELECT id FROM user WHERE account='"+req.body["username"]+"' AND password='"+req.body["password"]+"'", (err, result, fields)=>{
+      if(err) throw err;
+      if(result.length == 0){
+        res.json({"status": "login fail"})
+      }
+      result[0].type="buyer"
+      res.json(result[0])
+  });
+});
+
+app.post("/api/login/seller", (req,res,next)=>{
+  DB.query("SELECT id FROM company WHERE account='"+req.body["username"]+"' AND password='"+req.body["password"]+"'", (err, result, fields)=>{
+      if(err) throw err;
+      if(result.length == 0){
+        res.json({"status": "login fail"})
+      }
+      result[0].type="seller"
+      res.json(result[0])
+  });
+});
+
 app.get("/api/product/:id", (req,res,next)=>{
   DB.query("SELECT * FROM item WHERE id="+req.params.id, (err, result, fields)=>{
       if(err) throw err;
