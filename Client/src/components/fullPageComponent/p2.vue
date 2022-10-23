e8335dd8e80f4cf13bfc8b75012af6f7f6c57907
AlbertDoggyLin <albertlin2468@gmail.com>
2022-10-23 11:48:28 +0800

    try

--- Client/src/components/fullPageComponent/p2.vue
+++ Client/src/components/fullPageComponent/p2.vue
@@ -13,10 +13,8 @@ const pa=ref(null)
 const img=ref(null)
 const mounted=ref(false)
 const imgloaded=ref(false)
-const loaded=()=>{
-    imgloaded.value=true
-    if(mounted.value&&imgloaded.value){
-        setTimeout(() => {
+const adj=()=>{
+    setTimeout(() => {
             const ratio=img.value.clientHeight/img.value.clientWidth;
             const w=pa.value.clientWidth;
             const h=pa.value.clientHeight;
@@ -26,22 +24,21 @@ const loaded=()=>{
             img.value.style.width=`${picw}px`;
             img.value.style.height = `${picw*ratio}px`;
         }, 100);
+}
+const loaded=()=>{
+    imgloaded.value=true
+    if(mounted.value&&imgloaded.value){
+        adj()
     }
 }
 onMounted(()=>{
     mounted.value=true
     if(mounted.value&&imgloaded.value){
-        setTimeout(() => {
-            const ratio=img.value.clientHeight/img.value.clientWidth;
-            const w=pa.value.clientWidth;
-            const h=pa.value.clientHeight;
-            let picw=w<h/ratio?w:h/ratio;
-            if(picw==0)picw='50px';
-            console.log(w, h)
-            img.value.style.width=`${picw}px`;
-            img.value.style.height = `${picw*ratio}px`;
-        }, 100);
+        adj()
     }
+    window.addEventListener('resize', adj())
+    window.addEventListener('orientationchange', adj())
+
 })
 </script>
 
