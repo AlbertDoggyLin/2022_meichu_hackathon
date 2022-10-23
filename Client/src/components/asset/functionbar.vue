c68ab8cc8eb9993042bb5e59db99d6429a746c6c
AlbertDoggyLin <albertlin2468@gmail.com>
2022-10-23 08:06:44 +0800

    feat: link for but on navbar

--- Client/src/components/asset/functionbar.vue
+++ Client/src/components/asset/functionbar.vue
@@ -3,7 +3,7 @@
         <icon src='logo.svg'/>
         <div id="userName">{{ userName }}</div>
         <div id="placeName">{{ placeName }}</div>
-        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-cart-shopping" @click="$router.push('/user/shopping')"></i>
         <i class="fa-solid fa-sign-out"></i>
     </div>
 </template>
