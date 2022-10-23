e9d37d981d3ec6eae438d6b080110d90b8498e7f
AlbertDoggyLin <albertlin2468@gmail.com>
2022-10-23 11:59:40 +0800

    try

--- Client/src/components/fullPageComponent/p3.vue
+++ Client/src/components/fullPageComponent/p3.vue
@@ -1,12 +1,48 @@
 <template>
-    <div id="p3w">
+    <div id="p3w" ref="pa">
         <div>
-        <img src="/public/fullpage/p3.png" alt="p3">
+        <img src="/public/fullpage/p3.png" alt="p3" :onload="loaded()" ref="img">
         </div>
     </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue"
+
+const pa=ref(null)
+const img=ref(null)
+const mounted=ref(false)
+const imgloaded=ref(false)
+const loaded=()=>{
+    imgloaded.value=true
+    if(mounted.value&&imgloaded.value){
+        setTimeout(() => {
+            const ratio=img.value.clientHeight/img.value.clientWidth;
+            const w=pa.value.clientWidth;
+            const h=pa.value.clientHeight;
+            let picw=w<h/ratio?w:h/ratio;
+            if(picw==0)picw='50px';
+            console.log(w, h)
+            img.value.style.width=`${picw}px`;
+            img.value.style.height = `${picw*ratio}px`;
+        }, 100);
+    }
+}
+onMounted(()=>{
+    mounted.value=true
+    if(mounted.value&&imgloaded.value){
+        setTimeout(() => {
+            const ratio=img.value.clientHeight/img.value.clientWidth;
+            const w=pa.value.clientWidth;
+            const h=pa.value.clientHeight;
+            let picw=w<h/ratio?w:h/ratio;
+            if(picw==0)picw='50px';
+            console.log(w, h)
+            img.value.style.width=`${picw}px`;
+            img.value.style.height = `${picw*ratio}px`;
+        }, 100);
+    }
+})
 </script>
 
 <style scoped>
--- Client/src/components/fullPageComponent/p7.vue
+++ Client/src/components/fullPageComponent/p7.vue
@@ -1,12 +1,48 @@
 <template>
-    <div id="p7w">
+    <div id="p7w" ref="pa">
         <div>
-        <img src="/public/fullpage/p7.png" alt="p7">
+        <img src="/public/fullpage/p7.png" alt="p7" :onload="loaded()" ref="img">
         </div>
     </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue"
+
+const pa=ref(null)
+const img=ref(null)
+const mounted=ref(false)
+const imgloaded=ref(false)
+const loaded=()=>{
+    imgloaded.value=true
+    if(mounted.value&&imgloaded.value){
+        setTimeout(() => {
+            const ratio=img.value.clientHeight/img.value.clientWidth;
+            const w=pa.value.clientWidth;
+            const h=pa.value.clientHeight;
+            let picw=w<h/ratio?w:h/ratio;
+            if(picw==0)picw='50px';
+            console.log(w, h)
+            img.value.style.width=`${picw}px`;
+            img.value.style.height = `${picw*ratio}px`;
+        }, 100);
+    }
+}
+onMounted(()=>{
+    mounted.value=true
+    if(mounted.value&&imgloaded.value){
+        setTimeout(() => {
+            const ratio=img.value.clientHeight/img.value.clientWidth;
+            const w=pa.value.clientWidth;
+            const h=pa.value.clientHeight;
+            let picw=w<h/ratio?w:h/ratio;
+            if(picw==0)picw='50px';
+            console.log(w, h)
+            img.value.style.width=`${picw}px`;
+            img.value.style.height = `${picw*ratio}px`;
+        }, 100);
+    }
+})
 </script>
 
 <style scoped>
